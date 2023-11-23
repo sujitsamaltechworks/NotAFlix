0af00554c16d41730d189ad9d99022c0e918c96f
Sujit Samal <sujitofficial294@gmail.com>
2023-11-23 14:20:55 +0530

    added movie cards and detail pages

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import MovieList from "../MovieList/MovieList";
+import { useDispatch } from "react-redux";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../features/movies/movieSlice";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
+  return (
+    <div>
+      <div className="banner-img"></div>
+      <MovieList />
+    </div>
+  );
+};
+
+export default Home;
